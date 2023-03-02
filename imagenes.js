5c9ac3cf75af7572fcb1c2559d27c3be1a39389f
Fernando1809 <ferandre1718.frg@gmail.com>
2023-03-02 17:21:10 -0600

    2023-03-02

--- imagenes.js
+++ imagenes.js
@@ -1,19 +1,17 @@
-const img = (archivo, calidad) => {
-  return new Promise((resolve, reject) => {
-    const $canvas = document.createElement("canvas"),
-      imagen = new Image();
-    imagen.onload = () => {
-      $canvas.width = imagen.width;
-      $canvas.height = imagen.height;
+const img=(archivo, calidad)=>{
+  return new Promise((resolve, reject)=>{
+      const $canvas = document.createElement("canvas"),
+          imagen = new Image();
+      imagen.onload=()=>{
+          $canvas.width = imagen.width;
+          $canvas.height = imagen.height;
 
-      $canvas
-        .getContext("2d")
-        .drawImage(imagen, 0, 0, $canvas.with, $canvas.height);
-      $canvas.toBlob((blob) => {
-        if (blob == nulll) return reject(blob);
-        resolve(blob);
-      }, "image/jpeg", calidad);
-    };
-    imagen.src = URL.createObjectURL(archivo);
+          $canvas.getContext("2d").drawImage(imagen,0,0,$canvas.width, $canvas.height);
+          $canvas.toBlob(blob=>{
+              if(blob==null) return reject(blob);
+              resolve(blob);
+          }, "image/jpeg", calidad);
+      };
+      imagen.src = URL.createObjectURL(archivo);
   });
 };
--- matriculas.js
+++ matriculas.js
@@ -1,84 +1,75 @@
-Vue.component("v-select-alumnos", VueSelect.VueSelect);
-Vue.component("component-matriculas", {
-  data() {
-    return {
-      accion: "nuevo",
-      buscar: "",
-      matriculas: [],
-      alumnos: [],
-      matricula: {
-        idMatricula: "",
-        fecha: "",
-        pago: false,
-        comprobante :'',
-        alumno: {
-          id: "",
-          label: "",
-        },
-      },
-    };
-  },
-  methods: {
-    guardarMatricula() {
-      this.listar();
-      if (this.accion === "nuevo") {
-        this.matricula.idMatricula = new Date().getTime().toString(16);
-        this.matriculas.push(JSON.parse(JSON.stringify(this.matricula)));
-      } else if (this.accion === "modificar") {
-        let index = this.matriculas.findIndex(
-          (matricula) => matricula.idMatricula == this.matricula.idMatricula
-        );
-        this.matriculas[index] = JSON.parse(JSON.stringify(this.matricula));
-      } else if (this.accion === "eliminar") {
-        let index = this.matriculas.findIndex(
-          (matricula) => matricula.idMatricula == this.matricula.idMatricula
-        );
-        this.matriculas.splice(index, 1);
-      }
-      localStorage.setItem("matriculas", JSON.stringify(this.matriculas));
-      this.nuevoMatricula();
-    },
-    eliminarMatricula(matricula) {
-      if (confirm(`Esta seguro de eliminar a ${matricula.alumno.label}?`)) {
-        this.accion = "eliminar";
-        this.matricula = matricula;
-        this.guardarMatricula();
-      }
-    },
-    nuevoMatricula() {
-      this.accion = "nuevo";
-      this.matricula.idMatricula = "";
-      this.matricula.fecha = "";
-      this.matricula.pago = false;
-      this.matricula.alumno.id = "";
-      this.matricula.alumno.label = "";
-    },
-    modificarMatricula(matricula) {
-      this.accion = "modificar";
-      this.matricula = matricula;
-    },
-    listar() {
-      this.matriculas = JSON.parse(
-        localStorage.getItem("matriculas") || "[]"
-      ).filter(
-        (matricula) =>
-          matricula.alumno.label
-            .toLowerCase()
-            .indexOf(this.buscar.toLowerCase()) > -1 ||
-          matricula.fecha.indexOf(this.buscar) > -1
-      );
-      this.alumnos = JSON.parse(localStorage.getItem("alumnos") || "[]").map(
-        (alumno) => {
-          return { id: alumno.idAlumno, label: alumno.nombre };
+Vue.component('v-select-alumnos', VueSelect.VueSelect);
+Vue.component('component-matriculas',{
+    data() {
+        return {
+            accion:'nuevo',
+            buscar: '',
+            matriculas: [],
+            alumnos : [],
+            matricula:{
+                idMatricula  : '',
+                fecha     : '',
+                pago      : false,
+                comprobante : '',
+                alumno    : {
+                    id    : '',
+                    label : ''
+                },
+            }
         }
-      );
     },
-  },
-  template: `
+    methods:{
+        guardarMatricula(){
+            this.listar();
+            if(this.accion==='nuevo'){
+                this.matricula.idMatricula = new Date().getTime().toString(16);
+                this.matriculas.push( JSON.parse( JSON.stringify(this.matricula) ) );
+            }else if(this.accion==='modificar'){
+                let index = this.matriculas.findIndex(matricula=>matricula.idMatricula==this.matricula.idMatricula);
+                this.matriculas[index] = JSON.parse( JSON.stringify(this.matricula) );
+            }else if(this.accion==='eliminar'){
+                let index = this.matriculas.findIndex(matricula=>matricula.idMatricula==this.matricula.idMatricula);
+                this.matriculas.splice(index,1);
+            }
+            localStorage.setItem("matriculas", JSON.stringify(this.matriculas) );
+            this.nuevoMatricula();
+        },
+        eliminarMatricula(matricula){
+            if( confirm(`Esta seguro de eliminar a ${matricula.alumno.label}?`) ){
+                this.accion='eliminar';
+                this.matricula=matricula;
+                this.guardarMatricula();
+            }
+        },
+        nuevoMatricula(){
+            this.accion = 'nuevo';
+            this.matricula.idMatricula = '';
+            this.matricula.fecha = '';
+            this.matricula.pago = false;
+            this.matricula.alumno.id = '';
+            this.matricula.alumno.label = '';
+        },
+        modificarMatricula(matricula){
+            this.accion = 'modificar';
+            this.matricula = matricula;
+        },
+        listar(){
+            this.matriculas = JSON.parse( localStorage.getItem('matriculas') || "[]" )
+                .filter(matricula=>matricula.alumno.label.toLowerCase().indexOf(this.buscar.toLowerCase())>-1 ||
+                    matricula.fecha.indexOf(this.buscar)>-1);
+            this.alumnos = JSON.parse( localStorage.getItem('alumnos') || "[]" ).map(alumno=>{
+                return { 
+                    id: alumno.idAlumno,
+                    label : alumno.nombre
+                }
+            });
+        }
+    },
+    template: `
         <div class="row">
             <div class="col-12 col-md-6">
-                <div class="card text-center">
-                    <div class="card-header bg-warning">REGISTRO DE MATRICULA</div>
+                <div class="card">
+                    <div class="card-header">REGISTRO DE MATRICULA</div>
                     <div class="card-body">
                         <form id="frmMatricula" @reset.prevent="nuevoMatricula" v-on:submit.prevent="guardarMatricula">
                             <div class="row p-1">
@@ -106,14 +97,15 @@ Vue.component("component-matriculas", {
                                 </div>
                             </div>
                             <div class="row p-1">
-                            <div class="col-3 col-md-2">
-                            <img :src="matricula.comprobante" with="20" height="50">
-                            </div>
-                            <div class="col-9 col-md-10">
-                            <div class="input-group mb-3">
-                               <label class="input-group-text" for="inputGroupFile01">Upload</label>
-                                 <input accept="image/*" onChage="seleccionarImagen(this)" type="file" class="form-control" id="inputGroupFile01">
-                            </div>
+                                <div class="col-3 col-md-2">
+                                    <img :src="matricula.comprobante" width="50" height="50">
+                                </div>
+                                <div class="col-9 col-md-10">
+                                    <div class="input-group mb-3">
+                                        <label class="input-group-text" for="inputGroupFile01">Upload</label>
+                                        <input accept="image/*" onChange="seleccionarImagen(this)" type="file" class="form-control" id="inputGroupFile01">
+                                    </div>
+                                </div>
                             </div>
                             <div class="row p-1">
                                 <div class="col-3 col-md-3">
@@ -129,8 +121,8 @@ Vue.component("component-matriculas", {
                 </div>
             </div>
             <div class="col-12 col-md-6">
-                <div class="card text-center">
-                    <div class="card-header bg-warning">LISTADO DE MATRICULAS</div>
+                <div class="card">
+                    <div class="card-header">LISTADO DE MATRICULAS</div>
                     <div class="card-body">
                         <table class="table table-bordered table-hover">
                             <thead>
@@ -143,7 +135,9 @@ Vue.component("component-matriculas", {
                                 <tr>
                                     <th>FECHA</th>
                                     <th>PAGO</th>
-                                    <th colspan="2">ALUMNO</th>
+                                    <th>COMPROBANTE</th>
+                                    <th>ALUMNO</th>
+                                    <th>ELIMINAR</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -160,18 +154,18 @@ Vue.component("component-matriculas", {
                 </div>
             </div>
         </div>
-    `,
+    `
 });
-async function seleccionarImagen(){
-    let archivo = img.files[0];
+
+async function seleccionarImagen(image){
+    let archivo = image.files[0];
     if(archivo){
         let blob = await img(archivo, 1),
-        reader = new FileReader();
-        reader,onload = e=>{
-            app.$refs.matricula.matricula.comprobante =e.target.result;
-            console.log( e.target.result );       
+            reader = new FileReader();
+        reader.onload = e=>{
+            app.$refs.matricula.matricula.comprobante = e.target.result;
+            console.log( e.target.result ); 
         };
         reader.readAsDataURL(blob);
-
     }
 }
